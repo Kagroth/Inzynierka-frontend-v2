0ca05e176d4e9e40f8f2745c8e6c0008a57dface
Kamil Bania <kamilbania2@gmail.com>
2019-12-04 02:17:34 +0100

    Display test in expansion-panels

--- src/components/tasks/Task.vue
+++ src/components/tasks/Task.vue
@@ -1,49 +1,55 @@
 <template>
-    <div>
-      <v-container>
-        <v-row>
-          <v-col>
-            <h3> {{ task.title }} </h3>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            Przypisane do:
-          </v-col>
-        </v-row>
-        <v-row v-for="(group, index) in task.assignedTo" :key="index">
-          <v-col>
-             {{ group.name }}
-          </v-col>
-        </v-row>
-        <v-row v-if="task.taskType.name == 'Exercise'">
-          <v-col>
-            <exercise :pk="task.exercise.pk"></exercise>
-          </v-col>
-        </v-row>
-        <v-row v-else>
-          Kolokwium
-        </v-row>
-      </v-container>
-    </div>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col>
+          <h3>{{ task.title }}</h3>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>Przypisane do:</v-col>
+      </v-row>
+      <v-row v-for="(group, index) in task.assignedTo" :key="index">
+        <v-col>{{ group.name }}</v-col>
+      </v-row>
+      <v-row v-if="task.taskType.name == 'Exercise'">
+        <v-col>
+          <exercise :pk="task.exercise.pk"></exercise>
+        </v-col>
+      </v-row>
+      <v-row v-else>
+        <v-col cols="12">
+          <v-expansion-panels multiple="true">
+            <v-expansion-panel v-for="(exercise, index) in task.test.exercises" :key="index">
+              <v-expansion-panel-header>{{ exercise.title }}</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                {{ exercise.content }}
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <script>
-import Exercise from '@/components/exercises/Exercise'
+import Exercise from "@/components/exercises/Exercise";
 
 export default {
   components: {
-    'exercise': Exercise
+    exercise: Exercise
   },
 
   computed: {
-    task () {
-      let contextTask = this.$store.state.tasks.tasks.filter(task => task.pk === this.$route.params.pk)
+    task() {
+      let contextTask = this.$store.state.tasks.tasks.filter(
+        task => task.pk === this.$route.params.pk
+      );
       // filter zwraca tablicę, dlatego trzeba zwrócić pierwszy obiekt explicit
-      return contextTask[0]
+      return contextTask[0];
     }
   }
-}
+};
 </script>
 <style scoped>
-
 </style>
