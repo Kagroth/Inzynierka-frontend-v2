e376a4d6e748aa3eb4856fac15807561e277a709
Kamil Bania <kamilbania2@gmail.com>
2019-12-15 23:40:23 +0100

    Snackbar added, form refactored to v-card

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,38 +1,59 @@
 <template>
   <div>
-    <v-form>
-      <v-container>
-        <v-row>
-          <v-col>
-            <v-text-field type="text" v-model="form.username" label="Nazwa użytkownika" required></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-text-field type="password" v-model="form.password" label="Hasło" requried></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-btn color="primary" @click="loginUser">Zaloguj</v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
+    <v-snackbar v-model="snackbar.show" top :color="snackbar.color">
+      {{ snackbar.message }}
+      <v-btn @click="snackbar.show = false" text dark>Ok</v-btn>
+    </v-snackbar>
+    
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="6">
+          <v-card>
+            <v-card-title>Logowanie</v-card-title>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  type="text"
+                  v-model="form.username"
+                  label="Nazwa użytkownika"
+                  required
+                  outlined
+                ></v-text-field>
+                <v-text-field
+                  type="password"
+                  v-model="form.password"
+                  label="Hasło"
+                  requried
+                  outlined
+                ></v-text-field>
+                <v-btn color="primary" @click="loginUser" :loading="loading">Zaloguj</v-btn>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import { required } from 'vuelidate/lib/validators'
+import { required } from "vuelidate/lib/validators";
 
 export default {
-  data () {
+  data() {
     return {
+      loading: false,
+      snackbar: {
+        show: false,
+        color: "",
+        message: ""
+      },
+
       form: {
-        username: '',
-        password: ''
+        username: "",
+        password: ""
       }
-    }
+    };
   },
 
   validations: {
@@ -48,27 +69,43 @@ export default {
   },
 
   methods: {
-    loginUser (event) {
-      event.preventDefault()
+    loginUser(event) {
+      event.preventDefault();
 
       if (this.$v.$invalid) {
-        alert('Nie podano wszystkich danych logowania')
-        return
+        this.snackbar.message = "Nie podano wszystkich danych logowania";
+        this.snackbar.color = "warning";
+        this.snackbar.show = true;
+        return;
       }
 
-      this.$store
-        .dispatch('auth/loginUser', this.form)
-        .then(responseData => {
-          console.log(responseData.message)
-          alert(responseData.message)
+      this.loading = true;
 
-          if (responseData.data !== undefined && responseData.data.status === 200) {
-            this.$router.push('/')
-          }
-        })
+      this.$store.dispatch("auth/loginUser", this.form).then(responseData => {
+        this.loading = false;
+
+        if (
+          responseData.data !== undefined &&
+          responseData.data.status === 200
+        ) {
+          this.snackbar.message =
+            "Pomyslnie zalogowano. Nastąpi przekierowanie";
+          this.snackbar.color = "success";
+          this.snackbar.show = true;
+
+          setTimeout(() => {
+            this.$router.push("/");
+          }, 2000);
+        } else {
+          this.snackbar.message = responseData.message;
+          this.snackbar.color = "error";
+          this.snackbar.show = true;
+          return;
+        }
+      });
     }
   }
-}
+};
 </script>
 
 <style scoped>
