9e315f51cda3c8f5b2471f7ff380aea495129ed4
Kamil Bania <kamilbania2@gmail.com>
2020-01-11 21:22:45 +0100

    Navigation drawer rendering based on user type

--- src/components/StartSite.vue
+++ src/components/StartSite.vue
@@ -1,27 +1,62 @@
 <template>
   <div>
     <v-row>
-      <v-col cols="3" v-if="isLogged">
-        <v-card flat>
-          <v-navigation-drawer dark>
-            <v-list nav>
-              <v-list-item link to="/tasks">
+      <!-- Boczne menu -->
+      <v-col cols="2" v-if="isLogged">
+        <v-navigation-drawer dark>
+            <v-list nav>              
+              <v-list-item link :to="{name: 'MyTasks'}">
                 <v-list-item-icon>
-                  <v-icon>mdi-delete</v-icon>
+                  <v-icon>mdi-calendar-check</v-icon>
                 </v-list-item-icon>
                 <v-list-item-content>
-                  <v-list-item-title>Moje zadania</v-list-item-title>
+                  <v-list-item-title>Zadania</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
-              <v-list-item link to="/groups/groups">
+
+              <span v-if="userType.name === 'Teacher'">
+                <v-list-group prepend-icon="mdi-folder" nav>
+                  <template v-slot:activator>
+                    <v-list-item-title>Materialy</v-list-item-title>
+                  </template>
+                    <v-list-item link>
+                      <v-list-item-icon>
+                        <v-icon color="orange">mdi-file-document-box</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>Cwiczenia</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item link>                    
+                      <v-list-item-icon>
+                        <v-icon color="green">mdi-file-document-box-multiple</v-icon>
+                      </v-list-item-icon>
+                      <v-list-item-content>
+                        <v-list-item-title>Kolokwia</v-list-item-title>
+                      </v-list-item-content>
+                    </v-list-item>                  
+                </v-list-group>              
+              </span>
+
+              <v-list-item link :to="{name: 'MyGroups'}">
                 <v-list-item-icon>
-                  <v-icon>mdi-account</v-icon>
+                  <v-icon>mdi-account-group</v-icon>
                 </v-list-item-icon>
                 <v-list-item-content>
-                  <v-list-item-title>Moje grupy</v-list-item-title>
+                  <v-list-item-title>Grupy</v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
-              <v-list-item link to disabled>
+              
+               <v-list-item link v-if="userType.name === 'Teacher'" disabled>
+                  <v-list-item-icon>
+                    <v-icon>mdi-star</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Oceny</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+              <v-list-item v-else link disabled>
                 <v-list-item-icon>
                   <v-icon>mdi-magnify</v-icon>
                 </v-list-item-icon>
@@ -31,7 +66,6 @@
               </v-list-item>
             </v-list>
           </v-navigation-drawer>
-        </v-card>
       </v-col>
       <v-col>
         <router-view></router-view>
@@ -45,6 +79,10 @@ export default {
   computed: {    
     isLogged () {
       return this.$store.state.auth.isLogged
+    },
+
+    userType() {
+      return this.$store.state.auth.profile.userType;
     },
   }
 };
