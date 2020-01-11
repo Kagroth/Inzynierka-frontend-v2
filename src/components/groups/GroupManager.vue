e1862f33df7b679501748cb764393f8d92259c0f
Kamil Bania <kamilbania2@gmail.com>
2020-01-12 00:24:56 +0100

    Formatting chaned

--- src/components/groups/GroupManager.vue
+++ src/components/groups/GroupManager.vue
@@ -1,55 +1,49 @@
 <template>
-    <div>
-        <v-card>
-            <v-card-title>
-                <v-row v-if="currentRoute.name === 'GroupListing'">
-                    <v-col>
-                        Moje grupy
-                    </v-col>
-                    <v-spacer></v-spacer>
-                    <v-col>
-                        <v-btn color="success" :to="{name: 'GroupCreator'}">
-                            <v-icon>mdi-plus</v-icon>
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-card-title>
-            <v-card-text>
-                <router-view></router-view>                
-            </v-card-text>
-        </v-card>
-        
-    </div>
+  <div>
+    <v-card>
+      <v-card-title>
+        <v-row v-if="currentRoute.name === 'GroupListing'">
+          <v-col>Moje grupy</v-col>
+          <v-spacer></v-spacer>
+          <v-col>
+            <v-btn color="success" :to="{name: 'GroupCreator'}">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-title>
+      <v-card-text>
+        <router-view></router-view>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
-
 export default {
-    data() {
-        return {
-        }
-    },
+  data() {
+    return {};
+  },
 
-    created() {
-        this.$router.push({name: 'GroupListing'})
-    },
+  created() {
+    this.$router.push({ name: "GroupListing" });
+  },
 
-    watch: {
-        $route(to, from) {
-            if (to.name === 'MyGroups') {
-                this.$router.push({name: 'GroupListing'})
-            }
-        }
-    },
-
-    computed: {
-        currentRoute() {
-            return this.$route
-        }
+  watch: {
+    $route(to, from) {
+      if (to.name === "MyGroups") {
+        this.$router.push({ name: "GroupListing" });
+      }
     }
-}
+  },
+
+  computed: {
+    currentRoute() {
+      return this.$route;
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
