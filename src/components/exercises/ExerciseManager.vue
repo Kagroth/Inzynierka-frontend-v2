c6dcd4d8d976fb2d2728850689c377ce061d0854
Kamil Bania <kamilbania2@gmail.com>
2020-01-11 22:45:08 +0100

    Exercise and Test managers components

---
+++ src/components/exercises/ExerciseManager.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <exercise-listing></exercise-listing>
+    </div>
+</template>
+
+<script>
+import ExerciseListing from "@/components/exercises/ExerciseListing"
+
+export default {
+    components: {
+        ExerciseListing
+    }
+}
+</script>
---
+++ src/components/tests/TestManager.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <test-listing></test-listing>
+    </div>
+</template>
+
+<script>
+import TestListing from "@/components/tests/TestListing"
+
+export default {
+    components: {
+        TestListing
+    }    
+}
+</script>
