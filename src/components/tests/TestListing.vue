c9cab00c7894ef056764af6098338029e852e7dd
Kamil Bania <kamilbania2@gmail.com>
2020-01-12 19:53:41 +0100

    Filter tests by name

--- src/components/tests/TestListing.vue
+++ src/components/tests/TestListing.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+    <v-row>
+      <v-col>
+        <v-btn icon @click="showSearchField = !showSearchField" class="d-inline">
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-expandable-x-transition>
+          <v-text-field
+            v-if="showSearchField"
+            v-model="searchForName"
+            style="width: 20%; position: absolute; "
+            class="d-inline-flex ma-0 pa-0"
+            placeholder="Podaj nazwe kolokwium"
+          ></v-text-field>
+        </v-expandable-x-transition>
+      </v-col>
+    </v-row>
     <v-row v-for="(test, index) in tests" :key="index">
       <v-col>
         <h3>{{ test.title }}</h3>
@@ -13,11 +29,28 @@
 
 <script>
 export default {
+  data() {
+    return {
+      showSearchField: false,
+      searchForName: ""
+    };
+  },
+
   created() {
     this.$store.dispatch("tasks/getAllTests");
   },
 
   methods: {
+    filterTestsByName(testArray) {
+      if (this.searchForName === "") {
+        return testArray;
+      }
+
+      return testArray.filter(test => {
+        return test.title.includes(this.searchForName);
+      });
+    },
+
     showTestDetails(test) {
       this.$router.push({
         name: "TestDetails",
@@ -28,7 +61,7 @@ export default {
 
   computed: {
     tests() {
-      return this.$store.state.tasks.tests;
+      return this.filterTestsByName(this.$store.state.tasks.tests);
     }
   }
 };
