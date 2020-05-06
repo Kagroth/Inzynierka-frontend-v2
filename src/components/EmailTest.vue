6bf589cb615f007bea451166b985ee945fc90e5e
Kamil Bania <kamilbania2@gmail.com>
2020-05-07 01:00:45 +0200

    Email and GitHub API test component

--- src/components/EmailTest.vue
+++ src/components/EmailTest.vue
@@ -11,8 +11,22 @@
             <v-btn small @click="runTest(obj.data)">{{ obj.text }}</v-btn>
         </p>
         <h5>PUT</h5>
-        <p v-for="(obj, index) in testing_PUT" :key="index">
+        <p v-for="(obj, index) in testing_PUT" :key="`${index}-2`">
             <v-btn small @click="putTest(obj.data, obj.hash)">{{ obj.text }}</v-btn>
+        </p>
+        <v-divider></v-divider>
+        <br>
+        <h5>GitHub API Test</h5>
+        <br>
+        <p>
+            <v-btn @click="getRepos" small>
+                Pobranie repozytoriow
+            </v-btn>
+        </p>
+        <p>
+            <v-btn @click="getFilesOfRepo" small>
+                Pobranie plikow z repozytorium
+            </v-btn>
         </p>
     </div>
 </template>
@@ -106,6 +120,26 @@ export default {
                                         body: JSON.stringify(data)})
         
             console.log(response)
+        },
+
+        async getRepos() {
+            let response = await fetch('https://api.github.com/users/Kagroth/repos', {
+                method: 'GET'
+            })
+
+            let data = await response.json()
+
+            console.log(data)
+        },
+
+        async getFilesOfRepo() {
+            let response = await fetch('https://api.github.com/repos/Kagroth/NextGenMusic/contents/softwareengineering?ref=master', {
+                method: 'GET'
+            })
+
+            let data = await response.json()
+
+            console.log(data)
         }
     }
 }
