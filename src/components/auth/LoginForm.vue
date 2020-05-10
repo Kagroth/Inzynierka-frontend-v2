6f8205317cefca4c75c4ea66650dc3c5c3d5ce93
Kamil Bania <kamilbania2@gmail.com>
2020-05-11 01:23:05 +0200

    TextField input, blur actions and error messages and computed rules - form validation

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -4,7 +4,7 @@
       {{ snackbar.message }}
       <v-btn @click="snackbar.show = false" text dark>Ok</v-btn>
     </v-snackbar>
-    
+
     <v-container>
       <v-row justify="center">
         <v-col cols="6">
@@ -15,6 +15,9 @@
                 <v-text-field
                   type="text"
                   v-model="form.username"
+                  @input="$v.form.username.$touch()"
+                  @blur="$v.form.username.$touch()"
+                  :error-messages="usernameRules"
                   label="Nazwa użytkownika"
                   required
                   outlined
@@ -22,17 +25,22 @@
                 <v-text-field
                   type="password"
                   v-model="form.password"
+                  @input="$v.form.password.$touch()"
+                  @blur="$v.form.password.$touch()"
+                  :error-messages="passwordRules"
                   label="Hasło"
                   requried
                   outlined
                 ></v-text-field>
                 <p>
-                  <router-link
-                    :to="{name: 'ObtainResetPasswordLink'}"
-                  >Odzyskaj hasło</router-link>
-                  
+                  <router-link :to="{name: 'ObtainResetPasswordLink'}">Odzyskaj hasło</router-link>
                 </p>
-                <v-btn color="primary" @click="loginUser" :loading="loading" :disabled="disabled">Zaloguj</v-btn>
+                <v-btn
+                  color="primary"
+                  @click="loginUser"
+                  :loading="loading"
+                  :disabled="disabled"
+                >Zaloguj</v-btn>
               </v-form>
             </v-card-text>
           </v-card>
@@ -79,6 +87,8 @@ export default {
     loginUser(event) {
       event.preventDefault();
 
+      this.$v.$touch();
+
       if (this.$v.$invalid) {
         this.snackbar.message = "Nie podano wszystkich danych logowania";
         this.snackbar.color = "warning";
@@ -105,13 +115,35 @@ export default {
             this.$router.push("/");
           }, 2000);
         } else {
-
           this.snackbar.message = responseData.message;
           this.snackbar.color = "error";
           this.snackbar.show = true;
           return;
         }
       });
+    }
+  },
+
+  computed: {
+    usernameRules() {
+      const errors = [];
+
+      if (!this.$v.form.username.$dirty) return errors;
+
+      !this.$v.form.username.required &&
+        errors.push("Nie podano nazwy użytkownika");
+
+      return errors;
+    },
+
+    passwordRules() {
+      const errors = [];
+
+      if (!this.$v.form.password.$dirty) return errors;
+
+      !this.$v.form.password.required && errors.push("Nie podano hasła");
+
+      return errors;
     }
   }
 };
